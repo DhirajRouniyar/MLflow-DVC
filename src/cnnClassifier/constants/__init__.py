2c0e630fdcadcfdd65bc9435e52a060f60913bcf
Dhiraj Kumar Rouniyar <dkrouniyar@wpi.edu>
2025-09-07 01:54:58 -0500

    base_model_added

---
+++ src/cnnClassifier/constants/__init__.py
@@ -0,0 +1,7 @@
+#path that are constants
+#Its a general naming convention in python to write the constant names in all capital letters
+
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
